54703f1d5b66c9ba26578e5e2e582d98bbbec0df
paramjeet2003 <157730644+paramjeet2003@users.noreply.github.com>
2024-10-13 16:57:44 +0530

    Add files via upload

---
+++ app_gradio.py
@@ -0,0 +1,39 @@
+### first you need to create modelfile
+### and then create the model on your local system 
+### ollama create codepreet -f  modelfile
+import requests
+import json
+import gradio as gr
+
+url="http://localhost:7860/api/generate"
+headers={
+    'Content-Type':'application/json'
+}
+
+history=[]
+
+def generate_response(prompt):
+    history.append(prompt)
+    final_prompt="\n".join(history)
+    data={
+        "model":"codepreet",
+        "prompt":final_prompt,
+        "stream":False
+    }
+    response = requests.post(url,headers=headers, data=json.dumps(data), verify=False)
+    
+    if response.status_code==200:
+        response=response.text
+        data=json.loads(response)
+        actual_response=data['response']
+        return actual_response
+    else:
+        print("[-] Error:", response.text)
+
+interface=gr.Interface(
+    fn=generate_response,
+    inputs=gr.Textbox(lines=4,placeholder="Ask a Question"),
+    outputs="text"
+)
+
+interface.launch(share=True)
